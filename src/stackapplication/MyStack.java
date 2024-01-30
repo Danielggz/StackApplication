fa6c8060e88e71f3fe6b71dfa6abbf5f651f945c
Danielggz <dggz1993@gmail.com>
2024-01-30 10:48:34 +0000

    main classes with their functionality and gui design

---
+++ src/stackapplication/MyStack.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package stackapplication;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ *
+ * @author Daniel García
+ * 30 ene 2024
+ */
+public class MyStack implements StackInterface{
+    
+    ArrayList<Wagon> stackList;
+    
+    public MyStack() {
+        stackList = new ArrayList<>();
+    }
+
+    @Override
+    public void push(Object w) {
+        stackList.add(0, (Wagon) w); //Add wagon to the first position of the list
+    }
+
+    @Override
+    public Object pop() {
+        if(isEmpty()){
+            return null;
+        }else{
+            return stackList.remove(0); //Remove element on first position
+        }
+    }
+
+    @Override
+    public Object peek() {
+        if(isEmpty()){
+            return null;
+        }else{
+            return stackList.get(0); //Show element on first position
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return stackList.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return stackList.size();
+    }
+
+    @Override
+    public String displayStack() {
+        String output = "";
+        int counter = 0;
+        if(isEmpty()){
+            output += "There is no wagons attached to the train";
+            return output;
+        }else{
+            Iterator it = stackList.iterator();
+            while(it.hasNext()){
+                output += "Stack display: \n";
+                output += "Element " + counter + ": " + (String)it.next() + "\n";
+            }
+            return output;
+        }
+    }
+    
+}
---
+++ src/stackapplication/Wagon.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package stackapplication;
+
+/**
+ *
+ * @author Daniel García
+ * 30 ene 2024
+ */
+public class Wagon {
+    private String wagonName, productType;
+
+    public Wagon(String wagonName, String productType) {
+        this.wagonName = wagonName;
+        this.productType = productType;
+    }
+    
+    public void setWagonName(String wagonName) {
+        this.wagonName = wagonName;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public String getWagonName() {
+        return wagonName;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    @Override
+    public String toString() {
+        return "Wagon info: \n" + "Wagon name: " + wagonName + "\nProduct type: " + productType;
+    }
+}
